aa690a6e3a6947ee0bc69a92aa35a801c65585d4
Alexis Ducastel <alexis@infrabuilder.com>
2023-03-25 19:06:48 +0100

    transplaner-server fucntionnal

--- transplaneur-server/main.go
+++ transplaneur-server/main.go
@@ -9,11 +9,20 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"strconv"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 
+	"github.com/vishvananda/netlink"
+
 	iptables "github.com/coreos/go-iptables/iptables"
+
+	wgctrl "golang.zx2c4.com/wireguard/wgctrl"
+	wgtypes "golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 type IPAM struct {
@@ -45,7 +54,7 @@ func (ipam *IPAM) Register(publicKey string) (string, error) {
 	ipam.availableIPs = ipam.availableIPs[1:]
 	ipam.allocatedIPs[publicKey] = ip.String()
 
-	data, _ := json.Marshal(ipam.allocatedIPs)
+	data, _ := json.MarshalIndent(ipam.allocatedIPs, "", "  ")
 	ioutil.WriteFile(ipam.filePath, data, 0644)
 
 	return ip.String(), nil
@@ -53,12 +62,17 @@ func (ipam *IPAM) Register(publicKey string) (string, error) {
 
 type TransplaneurServer struct {
 	ipam         *IPAM
+	wgClient     *wgctrl.Client
 	wgDeviceName string
+	wgLink       *netlink.GenericLink
+	privateKey   string
+	publicKey    string
+	listenPort   int
 	gatewayIp    string
 	endpoint     string
 }
 
-func NewTransplaneurServer(wgDeviceName string, cidr string, filePath string, endpoint string) (*TransplaneurServer, error) {
+func NewTransplaneurServer(wgDeviceName string, privateKey string, listenPort int, cidr string, filePath string, endpoint string) (*TransplaneurServer, error) {
 
 	// === 1) Create the IPAM and set server IP ============================
 
@@ -108,9 +122,30 @@ func NewTransplaneurServer(wgDeviceName string, cidr string, filePath string, en
 
 	// === 2) Create the Transplaneur server ===============================
 
+	wgClient, err := wgctrl.New()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create wgctrl client: %v", err)
+	}
+
+	// Create a new WireGuard interface
+	wgLink := netlink.GenericLink{
+		LinkAttrs: netlink.LinkAttrs{Name: wgDeviceName},
+		LinkType:  "wireguard",
+	}
+
+	wgPrivateKey, err := wgtypes.ParseKey(privateKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse private key: %v", err)
+	}
+
 	return &TransplaneurServer{
 		ipam:         &ipam,
 		wgDeviceName: wgDeviceName,
+		wgLink:       &wgLink,
+		privateKey:   privateKey,
+		publicKey:    wgPrivateKey.PublicKey().String(),
+		listenPort:   listenPort,
+		wgClient:     wgClient,
 		gatewayIp:    gatewayIp.String(),
 		endpoint:     endpoint,
 	}, nil
@@ -124,18 +159,142 @@ func (ts *TransplaneurServer) GetEndpoint() string {
 	return ts.endpoint
 }
 
+func (ts *TransplaneurServer) GetGatewayPublicKey() string {
+	return ts.publicKey
+}
+
 func (ts *TransplaneurServer) RegisterClient(pubkey string) (string, error) {
-	return ts.ipam.Register(pubkey)
+
+	// Allocate an IP address
+	allocatedIp, err := ts.ipam.Register(pubkey)
+	if err != nil {
+		return "", err
+	}
+
+	// Create peer configuration
+
+	wgPublicKey, err := wgtypes.ParseKey(pubkey)
+	if err != nil {
+		return "", fmt.Errorf("Failed to parse public key: %v", err)
+	}
+
+	persistentKeepaliveInterval := time.Duration(25) * time.Second
+
+	peer := wgtypes.PeerConfig{
+		PublicKey:                   wgPublicKey,
+		ReplaceAllowedIPs:           true,
+		AllowedIPs:                  []net.IPNet{{IP: net.ParseIP(allocatedIp), Mask: net.CIDRMask(32, 32)}},
+		PersistentKeepaliveInterval: &persistentKeepaliveInterval,
+	}
+
+	// Configure the WireGuard interface
+	if err := ts.wgClient.ConfigureDevice(ts.wgDeviceName, wgtypes.Config{
+		Peers: []wgtypes.PeerConfig{peer},
+	}); err != nil {
+		return "", fmt.Errorf("failed to configure device: %v for peer %s (%s)", err, allocatedIp, pubkey)
+	}
+
+	// Return the allocated IP address
+	return allocatedIp, nil
 }
 
 func (ts *TransplaneurServer) Initiate() error {
 	ts.enableIPForwarding()
+	log.Println("IP forwarding enabled")
 	ts.natStart()
+	log.Println("NAT started")
+
+	peers := make([]wgtypes.PeerConfig, 0, len(ts.ipam.allocatedIPs))
+	for pubKeyStr, ipStr := range ts.ipam.allocatedIPs {
+		pubKey, err := wgtypes.ParseKey(pubKeyStr)
+		if err != nil {
+			return fmt.Errorf("failed to parse public key: %v", err)
+		}
+
+		ip := net.ParseIP(ipStr)
+		if ip == nil {
+			return fmt.Errorf("invalid IP: %s", ipStr)
+		}
+
+		persistentKeepaliveInterval := time.Duration(25) * time.Second
+
+		peers = append(peers, wgtypes.PeerConfig{
+			PublicKey: pubKey,
+			AllowedIPs: []net.IPNet{
+				{
+					IP:   ip,
+					Mask: net.CIDRMask(32, 32),
+				},
+			},
+			PersistentKeepaliveInterval: &persistentKeepaliveInterval,
+		})
+	}
+
+	// Convert the private key string to a wgtypes.Key
+	privateKey, err := wgtypes.ParseKey(ts.privateKey)
+
+	wgConfig := wgtypes.Config{
+		PrivateKey:   &privateKey,
+		ListenPort:   &ts.listenPort,
+		ReplacePeers: true,
+		Peers:        peers,
+	}
+
+	// Assuming the interface is already created using OS-specific method
+	// e.g., `ip link add dev wg0 type wireguard`
+
+	// Add the interface
+	err = netlink.LinkAdd(ts.wgLink)
+	if err != nil {
+		return fmt.Errorf("Error creating WireGuard interface: %v\n", err)
+	}
+
+	// Start the interface
+	err = netlink.LinkSetUp(ts.wgLink)
+	if err != nil {
+		return fmt.Errorf("Error starting WireGuard interface: %v\n", err)
+	}
+
+	// 1.1) Parse the CIDR
+	_, ipnet, err := net.ParseCIDR(ts.ipam.cidr)
+	if err != nil {
+		return fmt.Errorf("Error parsing CIDR: %v\n", err)
+	}
+
+	// Create an Addr object
+	addr := &netlink.Addr{
+		IPNet: &net.IPNet{
+			IP:   net.ParseIP(ts.gatewayIp),
+			Mask: ipnet.Mask,
+		},
+	}
+
+	// Add the IP address to the interface
+	err = netlink.AddrAdd(ts.wgLink, addr)
+	if err != nil {
+		return fmt.Errorf("Error adding IP address: %v\n", err)
+	}
+
+	// Configure the wireguard interface
+	err = ts.wgClient.ConfigureDevice(ts.wgDeviceName, wgConfig)
+	if err != nil {
+		return fmt.Errorf("failed to configure WireGuard device: %v", err)
+	}
+
+	log.Println("WireGuard configured")
+
 	return nil
 }
 
 func (ts *TransplaneurServer) Shutdown() error {
 	ts.natStop()
+	log.Println("NAT stopped")
+
+	err := netlink.LinkDel(ts.wgLink)
+	if err != nil {
+		return fmt.Errorf("Error stopping WireGuard interface: %v\n", err)
+	}
+
 	return nil
 }
 
@@ -204,154 +363,15 @@ func nextIP(ip net.IP) net.IP {
 	return net.IPv4(v0, v1, v2, v3)
 }
 
-// type WireGuardInterface struct {
-// 	deviceName string
-// 	client     *wgctrl.Client
-// 	ipam       *IPAM
-// }
-
-// func NewWireGuardInterface(deviceName string, ipam *IPAM) (*WireGuardInterface, error) {
-// 	client, err := wgctrl.New()
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to create wgctrl client: %v", err)
-// 	}
-
-// 	return &WireGuardInterface{
-// 		deviceName: deviceName,
-// 		client:     client,
-// 		ipam:       ipam,
-// 	}, nil
-// }
-
-// func (wgi *WireGuardInterface) Initiate() error {
-// 	// Assuming the interface is already created using OS-specific method
-// 	// e.g., `ip link add dev wg0 type wireguard`
-
-// 	peers, err := wgi.buildPeers()
-// 	if err != nil {
-// 		return fmt.Errorf("failed to build peers: %v", err)
-// 	}
-
-// 	config := wgtypes.Config{
-// 		ReplacePeers: true,
-// 		Peers:        peers,
-// 	}
-
-// 	return wgi.client.ConfigureDevice(wgi.deviceName, config)
-// }
-
-// func (wgi *WireGuardInterface) buildPeers() ([]wgtypes.PeerConfig, error) {
-// 	wgi.ipam.mu.Lock()
-// 	defer wgi.ipam.mu.Unlock()
-
-// 	peers := make([]wgtypes.PeerConfig, 0, len(wgi.ipam.allocatedIPs))
-
-// 	for pubKeyStr, ipStr := range wgi.ipam.allocatedIPs {
-// 		pubKey, err := wgtypes.ParseKey(pubKeyStr)
-// 		if err != nil {
-// 			return nil, fmt.Errorf("failed to parse public key: %v", err)
-// 		}
-
-// 		ip := net.ParseIP(ipStr)
-// 		if ip == nil {
-// 			return nil, fmt.Errorf("invalid IP: %s", ipStr)
-// 		}
-
-// 		peers = append(peers, wgtypes.PeerConfig{
-// 			PublicKey: pubKey,
-// 			AllowedIPs: []net.IPNet{
-// 				{
-// 					IP:   ip,
-// 					Mask: net.CIDRMask(32, 32),
-// 				},
-// 			},
-// 			PersistentKeepaliveInterval: time.Duration(25 * time.Second),
-// 		})
-// 	}
-
-// 	return peers, nil
-// }
-
-// func (wgi *WireGuardInterface) Shutdown() error {
-// 	// Assuming you remove the interface using OS-specific method
-// 	// e.g., `ip link del dev wg0`
-
-// 	return wgi.client.Close()
-// }
-
-// func (wgi *WireGuardInterface) AddPeer(pubKeyStr string, ipStr string) error {
-// 	pubKey, err := wgtypes.ParseKey(pubKeyStr)
-// 	if err != nil {
-// 		return fmt.Errorf("failed to parse public key: %v", err)
-// 	}
-
-// 	ip := net.ParseIP(ipStr)
-// 	if ip == nil {
-// 		return fmt.Errorf("invalid IP: %s", ipStr)
-// 	}
-
-// 	peerConfig := wgtypes.PeerConfig{
-// 		PublicKey: pubKey,
-// 		AllowedIPs: []net.IPNet{
-// 			{
-// 				IP:   ip,
-// 				Mask: net.CIDRMask(32, 32),
-// 			},
-// 		},
-// 		PersistentKeepaliveInterval: time.Duration(25 * time.Second),
-// 	}
-
-// 	err = wgi.client.ConfigureDevice(wgi.deviceName, wgtypes.Config{
-// 		ReplacePeers: false,
-// 		Peers:        []wgtypes.PeerConfig{peerConfig},
-// 	})
-
-// 	if err == nil {
-// 		wgi.ipam.mu.Lock()
-// 		wgi.ipam.allocatedIPs[pubKeyStr] = ipStr
-// 		wgi.ipam.mu.Unlock()
-// 	}
-
-// 	return err
-// }
-
-// func (wgi *WireGuardInterface) RemovePeer(pubKeyStr string) error {
-// 	pubKey, err := wgtypes.ParseKey(pubKeyStr)
-// 	if err != nil {
-// 		return fmt.Errorf("failed to parse public key: %v", err)
-// 	}
-
-// 	peerConfig := wgtypes.PeerConfig{
-// 		PublicKey: pubKey,
-// 		Remove:    true,
-// 	}
-
-// 	err = wgi.client.ConfigureDevice(wgi.deviceName, wgtypes.Config{
-// 		ReplacePeers: false,
-// 		Peers:        []wgtypes.PeerConfig{peerConfig},
-// 	})
-
-// 	if err == nil {
-// 		wgi.ipam.mu.Lock()
-// 		ipStr, ok := wgi.ipam.allocatedIPs[pubKeyStr]
-// 		if ok {
-// 			delete(wgi.ipam.allocatedIPs, pubKeyStr)
-// 			wgi.ipam.availableIPs = append(wgi.ipam.availableIPs, net.ParseIP(ipStr))
-// 		}
-// 		wgi.ipam.mu.Unlock()
-// 	}
-
-// 	return err
-// }
-
 type RegisterRequest struct {
 	PublicKey string `json:"publickey"`
 }
 
 type RegisterResponse struct {
-	ClientIP  string `json:"client_ip"`
-	GatewayIP string `json:"gateway_ip"`
-	Endpoint  string `json:"endpoint"`
+	ClientIP         string `json:"client_ip"`
+	GatewayIP        string `json:"gateway_ip"`
+	Endpoint         string `json:"endpoint"`
+	GatewayPublicKey string `json:"gateway_public_key"`
 }
 
 func httpPostRegister(ts *TransplaneurServer, w http.ResponseWriter, r *http.Request) {
@@ -374,9 +394,10 @@ func httpPostRegister(ts *TransplaneurServer, w http.ResponseWriter, r *http.Req
 	}
 
 	res := RegisterResponse{
-		GatewayIP: ts.GetGatewayIP(),
-		ClientIP:  ip,
-		Endpoint:  ts.GetEndpoint(),
+		GatewayIP:        ts.GetGatewayIP(),
+		ClientIP:         ip,
+		Endpoint:         ts.GetEndpoint(),
+		GatewayPublicKey: ts.GetGatewayPublicKey(),
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -386,7 +407,18 @@ func httpPostRegister(ts *TransplaneurServer, w http.ResponseWriter, r *http.Req
 func main() {
 	//====/ Configuration \=============================================
 
-	// Retrieve configuration from environment variables
+	// Mandatory environment variables
+	privateKey := os.Getenv("WG_PRIVATE_KEY")
+	if privateKey == "" {
+		log.Fatal("WG_PRIVATE_KEY is not set")
+	}
+
+	wgServerEndpoint := os.Getenv("WG_ENDPOINT")
+	if privateKey == "" {
+		log.Fatal("WG_ENDPOINT is not set")
+	}
+
+	// Optional environment variables
 	cidr := "10.242.0.0/16"
 	if os.Getenv("CIDR") != "" {
 		cidr = os.Getenv("CIDR")
@@ -402,27 +434,63 @@ func main() {
 		wgInterfaceName = os.Getenv("WG_INTERFACE_NAME")
 	}
 
-	port := "8080"
-	if os.Getenv("PORT") != "" {
-		port = os.Getenv("PORT")
+	httpPort := "8080"
+	if os.Getenv("HTTP_LISTEN_PORT") != "" {
+		httpPort = os.Getenv("HTTP_LISTEN_PORT")
+	}
+
+	wgPort := 51820
+	if os.Getenv("WG_LISTEN_PORT") != "" {
+		parsedWgPort, err := strconv.Atoi(os.Getenv("WG_LISTEN_PORT"))
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			wgPort = parsedWgPort
+		}
 	}
 
 	//====/ Transplaneur \==============================================
 
-	transplaneurServer, err := NewTransplaneurServer(wgInterfaceName, cidr, filePath, "")
+	transplaneurServer, err := NewTransplaneurServer(wgInterfaceName, privateKey, wgPort, cidr, filePath, wgServerEndpoint)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	transplaneurServer.Initiate()
+	//Initialize IPAM, wireguard, etc.
+	err = transplaneurServer.Initiate()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//====/ Signal handling \============================================
+
+	// Create a channel to receive the signals
+	signalChan := make(chan os.Signal, 1)
+
+	// Register the signals to be caught
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start a goroutine to handle the signals
+	go func() {
+		sig := <-signalChan
+		fmt.Printf("\nReceived signal: %s\n", sig)
+
+		// Perform your clean-up action here
+		fmt.Println("Performing clean-up...")
+
+		transplaneurServer.Shutdown()
+
+		// Exit the program
+		os.Exit(0)
+	}()
 
 	//====/ HTTP API \======================================================
 
 	// Create API server
-	r := mux.NewRouter()
+	api := mux.NewRouter()
 
 	// Check bearer as middleware
-	r.Use(func(next http.Handler) http.Handler {
+	api.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			token := r.Header.Get("Authorization")
 			if token != "Bearer "+os.Getenv("BEARER_TOKEN") {
@@ -434,7 +502,7 @@ func main() {
 	})
 
 	// Log requests as middleware
-	r.Use(func(next http.Handler) http.Handler {
+	api.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			log.Printf("%s %s %s %s %s", r.RemoteAddr, r.Method, r.URL, r.Proto, r.UserAgent())
 			next.ServeHTTP(w, r)
@@ -442,11 +510,12 @@ func main() {
 	})
 
 	// Endpoint to register a new peer
-	r.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+	api.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 		httpPostRegister(transplaneurServer, w, r)
 	}).Methods("POST")
 
 	// Start API server
-	log.Printf("Starting server on port %s...\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	log.Printf("Starting server on port %s...\n", httpPort)
+	log.Fatal(http.ListenAndServe(":"+httpPort, api))
+
 }
